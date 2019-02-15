912c1d2193b3b10302e08a6beb5d73ac6b1faba9
GitInMaRepo <oliver.s.roth@gmail.com>
2019-02-15 15:20:00 +0100

    first test

--- raindrops.test.ts
+++ raindrops.test.ts
@@ -1,1 +1,8 @@
 import RainDrops from "./raindrops"
+
+describe('Raindrops', () => {
+    it('should say "1" if we give it a 1', () => {
+        const target = new RainDrops()
+        expect(target.rain(1)).toBe("1")
+    })
+})
--- raindrops.ts
+++ raindrops.ts
@@ -1,4 +1,7 @@
 class RainDrops {
+    rain(input: number): string {
+        return "1"
+    }
 }
 
 export default RainDrops
